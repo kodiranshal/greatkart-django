ea528040e07f408d354b7db4589894d8eb2496c2
kodiranshal <kodiranshal@gmail.com>
2025-01-21 17:19:06 +0700

    cart.htmk

--- store/models.py
+++ store/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 
-
-# Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
@@ -12,14 +10,36 @@ class Product(models.Model):
     images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
-    category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
-#link ke deskripsi
-    def get_url (self):
-        return reverse('product_detail', args=[self.category.slug, self.slug])  # Assuming 'product_detail' is the name of your product detail view and 'category.slug' and'self.slug' are the slugs of the category and the product, respectively.  # If your view name is different or the slugs are different, you will need to adjust this line accordingly.  # This will return the URL to the product detail page.  # You can then use this URL in your template to link to the product detail page.  # For example, in your template, you can use: <a href="{{ product.get_url }}">View Product</a>.  # This will link to the product detail page for the given product.  # If you want to link to the product detail page for all products in a given category, you can use: <a href="{{ category.get_url }}">View All Products</a
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super (VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def sizes(self):
+        return super (VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size','size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)  # Fixed typo in field name
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()  # Using VariationManager instead of models.Manager to filter colors and sizes. This is a more efficient way to filter variations.
+
+    def __str__(self):
+        return self.variation_value  # Changed to return variation_value instead of product
--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -10,13 +15,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,  # Passing all available products to the template.  # This is where you can modify the template to display the products in a more meaningful way.  # For example, you can loop over the products in the template and create HTML elements to display each product.  # Also, you can add any other necessary context variables to pass to the template.  # For example, you can pass the total number of products available to the template.  # The template should then be able to access these context variables and display the products and other relevant information.  # If you don't need to modify the template, you can just pass the products variable directly to the render function.  # For example, render(request, 'home.html', {'products': products}) instead of render(request, 'home.html') and then in the template, you can access the products variable using {{ products }}.  # If you need to add more context variables
+        'products': paged_products,  # Passing all available products to the template.  # This is where you can modify the template to display the products in a more meaningful way.  # For example, you can loop over the products in the template and create HTML elements to display each product.  # Also, you can add any other necessary context variables to pass to the template.  # For example, you can pass the total number of products available to the template.  # The template should then be able to access these context variables and display the products and other relevant information.  # If you don't need to modify the template, you can just pass the products variable directly to the render function.  # For example, render(request, 'home.html', {'products': products}) instead of render(request, 'home.html') and then in the template, you can access the products variable using {{ products }}.  # If you need to add more context variables
         'product_count': product_count,
     }
     return render (request, 'store/store.html', context)
@@ -24,11 +35,28 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+       
     except Exception as e:
         raise e 
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,  # This variable is used to determine if the current product is already in the user's shopping cart.  # If the product is in the cart, the variable will be True.  # If the product is not in the cart, the variable will be False.  # This variable is then used in the template to display a different message or button for the user to add the product to their cart.  # You can modify the template to
     }
     
     return render(request, 'store/product_detail.html', context)
+
+def search (request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    
+    return render (request, 'store/store.html', context)
+    # return HttpResponse("Search results")
